778e81f2c1bb48a3354f9bf80a1b623853dc2077
Pavithra354 <naralapavithra@gmail.com>
2025-05-20 13:13:18 +0530

    function

--- functions.js
+++ functions.js
@@ -3,3 +3,9 @@ function studentDetails() {
     console.log(studentName);
 }
 studentDetails()
+
+function studentDetails() {
+    let studentName = "Swapna";
+    console.log(studentName);
+}
+studentDetails()
