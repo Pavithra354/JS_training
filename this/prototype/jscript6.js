d02f9d0db9832252a4ed82426b5199d93674f731
Pavithra354 <naralapavithra@gmail.com>
2025-05-27 21:43:30 +0530

    commit

--- this/prototype/jscript6.js
+++ this/prototype/jscript6.js
@@ -5,7 +5,7 @@ function Phone(){
     
 }
 
-Phone.getPhoneInfo = function() {
+Phone. prototype.getPhoneInfo = function() {
     console.log("getPhoneInfo this refers to", this);
     console.log(`Phone Brand is ${this.brand} and model is ${this.modelName}`);
 };
