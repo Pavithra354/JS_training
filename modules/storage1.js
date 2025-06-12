fc4808ae4557e25e29d9d1897d97eb9567593ed3
Pavithra354 <naralapavithra@gmail.com>
2025-06-12 21:55:17 +0530

    commit

---
+++ modules/storage1.js
@@ -0,0 +1,9 @@
+//local storage
+
+localStorage.setItem("username","pavithra");
+localStorage.setItem("bankName","hdfc")
+localStorage.setItem("userpassword",1234)
+
+// localStorage.clear();
+// localStorage.removeItem("username");
+// localStorage.removeItem("userpassword");
